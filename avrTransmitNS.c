d3e5b1d18b297f1ec56f7792ffb608e92a82f8de
Programistanamiare <szymon.ferdyniok@gmail.com>
2025-09-03 20:13:19 +0200

    Add 14 - 16 MHz support transmition

--- avrTransmitNS.c
+++ avrTransmitNS.c
@@ -7,118 +7,117 @@ void transmitNS(volatile uint8_t *data, volatile uint16_t data_l, const gpio_pin
   register uint8_t bit_count asm("r17");
   const uint8_t hi = *p->port | (1 << p->pin), lo = *p->port & ~(1 << p->pin);
 
-#if (F_CPU >= 14000000) && (F_CPU <= 20000000)
+#if (F_CPU >= 14000000) && (F_CPU <= 16000000)
   asm volatile(
     // Init
-    "ld %[bit_count], 0x08 \n\t"
+    "ldi %[bit_count], 0x08 \n\t"
     "ld %[color], %a[data]+ \n\t"
     "lsl %[color] \n\t"
-    "brcc 0f \n\t"
+    "brcc 1f \n\t"
     "rjmp 4f \n\t"
 
-    // Last state 1 [ 2L | 13H | 4/5L ]
-    "5: \n\t"
-    "st %[port], %[hi] \n\t"          // 2 prev -> set to hi
-    "ldi %[bit_count], 0x08 \n\t"     // 1
-    "nop \n\t"                        // 2
-    "nop \n\t"                        // 3
-    "nop \n\t"                        // 4
-    "nop \n\t"                        // 5
-    "nop \n\t"                        // 6
-    "nop \n\t"                        // 7
-    "sbiw %[data_l], 0x01 \n\t"       // 9
-    "breq 6f \n\t"                    // 10 | 11 if jump
-    "nop \n\t"                        // 11
-    "st %[port], %[lo] \n\t"          // 13 -> set to lo
-    "nop \n\t"                        // 1
-    "lsl %[color] \n\t"               // 2
-    "brcc 0b \n\t"                    // 3 | 4 if jump
-    "rjmp 4b \n\t"                    // 5
+    // Last bit 1 [ 13H | 8L ]
+    "0: \n\t"
+    "st %a[port], %[hi] \n\t"           // 2 prev -> set to hi
+    "ldi %[bit_count], 0x08 \n\t"       // 1
+    "nop \n\t"                          // 2
+    "nop \n\t"                          // 3
+    "nop \n\t"                          // 4
+    "nop \n\t"                          // 5
+    "nop \n\t"                          // 6
+    "nop \n\t"                          // 7
+    "sbiw %[data_l], 0x01 \n\t"         // 9
+    "breq 2f \n\t"                      // 10 | 11 if jump
+    "nop \n\t"                          // 11
+    "st %a[port], %[lo] \n\t"           // 13 -> set to lo
+    "ld %[color], %a[data]+ \n\t"       // 2
+    "lsl %[color] \n\t"                 // 3
+    "brcc 1f \n\t"                      // 4 | 5 if jump
+    "rjmp 4f \n\t"                      // 6
 
-    // State 0 [ 3L | 6H | 11/12L ]
-    "0: \n\t"                         
-    "nop \n\t"                        // 1 prev
-    "st %a[port], %[hi] \n\t"         // 3 prev -> set to hi
-    "nop \n\t"                        // 1
-    "nop \n\t"                        // 2
-    "nop \n\t"                        // 3
-    "nop \n\t"                        // 4
-    "st %a[port], %[lo] \n\t"         // 6 -> set to lo
-    "nop \n\t"                        // 1
-    "nop \n\t"                        // 2
-    "nop \n\t"                        // 3
-    "nop \n\t"                        // 4
-    "dec %[bit_count] \n\t"           // 5
-    "cpi %[bit_count], 0x01 \n\t"     // 6
-    "breq 1f \n\t"                    // 7 | 8 if jump to 
-    "nop \n\t"                        // 8
-    "lsl %[color] \n\t"               // 9
-    "brcc 0b \n\t"                    // 10 | 11 if jump [ State 0 ]
-    "rjmp 4f \n\t"                    // 12 jump to [ State 1 ]
-
-    // Last byte bridge from state 0 (end with 8)
+    // State 0 [ 7H | 13L ]
     "1: \n\t"
-    "lsl %[color] \n\t"               // 9
-    "brcc 2f \n\t"                    // 10 | 11 if jump
-    "rjmp 5f \n\t"                    // 12
+    "nop \n\t"                          // 1 prev
+    "st %a[port], %[hi] \n\t"           // 3 prev -> set to hi
+    "nop \n\t"                          // 1
+    "nop \n\t"                          // 2
+    "nop \n\t"                          // 3
+    "nop \n\t"                          // 4
+    "nop \n\t"                          // 5
+    "st %a[port], %[lo] \n\t"           // 7 -> set to lo
+    "nop \n\t"                          // 1
+    "nop \n\t"                          // 2
+    "nop \n\t"                          // 3
+    "dec %[bit_count] \n\t"             // 4
+    "cpi %[bit_count] \n\t"             // 5
+    "breq 3f \n\t"                      // 6 | 7 if jump
+    "nop \n\t"                          // 7
+    "lsl %[color] \n\t"                 // 8
+    "brcc 1b \n\t"                      // 9 | 10 if jump
+    "rjmp 4f \n\t"                      // 11
 
-    // Last state 0 [ 3L | 6H | 11/12L ]
+    // Bridge to end
     "2: \n\t"
-    "nop \n\t"                        // 1 prev
-    "st %[port], %[hi] \n\t"          // 3 prev -> set to hi
-    "ldi %[bit_count], 0x08 \n\t"     // 1
-    "nop \n\t"                        // 2
-    "nop \n\t"                        // 3
-    "nop \n\t"                        // 4
-    "st %[port], %[lo] \n\t"          // 6 -> set to lo
-    "nop \n\t"                        // 1
-    "nop \n\t"                        // 2
-    "nop \n\t"                        // 3
-    "sbiw %[data_l], 0x01 \n\t"       // 5
-    "breq 6f \n\t"                    // 6 | 7 if jump
-    "ld %[color], %a[data]+ \n\t"     // 8
-    "lsl %[color] \n\t"               // 9
-    "brcc 0b \n\t"                    // 10 | 11 if jump
-    "rjmp 4b \n\t"                    // 12
+    "st %a[port], %[lo] \n\t"
+    "rjmp 6f \n\t"
 
-    // Last byte bridge from state 1 (end with 11)
+    // Last bit bridge
     "3: \n\t"
-    "st %[port], %[lo] \n\t"          // 13 -> set to low
-    "nop \n\t"                        // 1
-    "lsl %[color] \n\t"               // 2
-    "brcc 2b \n\t"                    // 3 | 4 if jump
-    "rjmp 5f \n\t"                    // 5
-    
-    // State 1 [ 2L | 13H | 4/5L ]
-    "4: \n\t"
-    "st %[port], %[hi] \n\t"          // 2 prev -> set to hi
-    "nop \n\t"                        // 1
-    "nop \n\t"                        // 2
-    "nop \n\t"                        // 3
-    "nop \n\t"                        // 4
-    "nop \n\t"                        // 5
-    "nop \n\t"                        // 6
-    "nop \n\t"                        // 7
-    "dec %[bit_count] \n\t"           // 8
-    "cpi %[bit_count], 0x01 \n\t"     // 9
-    "breq 2b \n\t"                    // 10 | 11 if jump
-    "nop \n\t"                        // 11
-    "st %[port], %[lo] \n\t"          // 13 -> set to lo
-    "nop \n\t"                        // 1
-    "lsl %[color] \n\t"               // 2
-    "brcc 0b \n\t"                    // 3 | 4 if jump
-    "rjmp 4b \n\t"                    // 5
+    "lsl %[color] \n\t"                  
+    "brcc 5f \n\t"                       
+    "rjmp 0b \n\t"                      
 
-    // End transmit (set low, and wait 3/4 nop)
+    // State 1 [ 13H | 8L ]
+    "4: \n\t"
+    "st %a[port], %[hi] \n\t"           // 2 prev -> set to hi
+    "nop \n\t"                          // 1
+    "nop \n\t"                          // 2
+    "nop \n\t"                          // 3
+    "nop \n\t"                          // 4
+    "nop \n\t"                          // 5
+    "nop \n\t"                          // 6
+    "nop \n\t"                          // 7
+    "nop \n\t"                          // 8
+    "nop \n\t"                          // 9
+    "dec %[bit_count] \n\t"             // 10
+    "cpi %[bit_count], 0x01 \n\t"       // 11
+    "st %a[port], %[lo] \n\t"           // 13 -> set to lo
+    "breq 3b \n\t"                      // 1 | 2 if jump
+    "nop \n\t"                          // 2
+    "lsl %[color] \n\t"                 // 3
+    "brcc 1b \n\t"                      // 4 | 5 if jump
+    "rjmp 4b \n\t"                      // 6
+
+    // Last bit 0 [ 7H | 13 L ]
+    "5: \n\t"
+    "nop \n\t"                          // 1 prev
+    "st %a[port], %[hi] \n\t"           // 3 prev -> set to hi
+    "ldi %[bit_count], 0x08 \n\t"       // 1
+    "nop \n\t"                          // 2
+    "nop \n\t"                          // 3
+    "nop \n\t"                          // 4
+    "nop \n\t"                          // 5
+    "st %a[port], %[lo] \n\t"           // 7 -> set to lo
+    "nop \n\t"                          // 1
+    "nop \n\t"                          // 2
+    "sbiw %[data_l], 0x01 \n\t"         // 4
+    "breq 6f \n\t"                      // 5 | 6 if jump
+    "ld %[color], %a[data]+ \n\t"       // 7
+    "lsl %[color] \n\t"                 // 8
+    "brcc 1b \n\t"                      // 9 | 10 if jump
+    "rjmp 4b \n\t"                      // 11
+
+    // END
     "6: \n\t"
-    "st %[port], %[lo] \n\t"
     "nop \n\t"
     "nop \n\t"
     "nop \n\t"
     "nop \n\t"
+    "nop \n\t"
+    "nop \n\t"
+    "nop \n\t"
+    
   );
-#elif (F_CPU >= 5500000) && (F_CPU <= 8500000)
-  
 #else
 #error "Unsupported F_CPU!"
 #endif
